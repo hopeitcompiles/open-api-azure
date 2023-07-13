13ad8e908da73b13e04cf6a21347e7a4adf25448
Cristhian Castro <cristhian.castro.lucas@udla.edu.ec>
2023-07-13 00:46:22 -0500

    w

--- OpenAI/index.js
+++ OpenAI/index.js
@@ -2,8 +2,7 @@
   context.log("JavaScript HTTP trigger function processed a request.");
 
   const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = "It's working";
-
+  const responseMessage = "it works";
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: responseMessage,
